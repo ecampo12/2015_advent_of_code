e5a60e21d5768ebe3646f59711c6c55ef4ad61fd
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-27 22:06:53 -0500

    Created by script: Day15

---
+++ day15/test.py
@@ -0,0 +1,19 @@
+import unittest
+from AOC import *
+
+class Test(unittest.TestCase):
+    def test_part1(self):
+        file = open("test_input.txt", "r")
+        input = file.read().splitlines()
+        file.close()
+        self.assertTrue(False)
+
+    def test_part2(self):
+        file = open("test_input.txt", "r")
+        input = file.read().splitlines()
+        file.close()
+        self.assertTrue(False)
+        
+if __name__ == "__main__":
+    unittest.main()
+    
