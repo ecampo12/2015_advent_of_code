129f619f238af536bc71b752b5abd2d2d426df9f
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-30 23:46:02 -0500

    Created by script: Day23

---
+++ day23/AOC.py
@@ -0,0 +1,20 @@
+import re
+
+def parse_input(input):
+    return input
+
+def part1(input):
+    return True
+
+def part2(input):
+    return True
+
+def main():
+    # input = open("input.txt", "r").read().splitlines()
+    part1_ans = 0
+    print(f"Part 1: {part1_ans}")
+    part2_ans = 0
+    print(f"Part 2: {part2_ans}")
+
+if __name__ == "__main__":
+    main()
