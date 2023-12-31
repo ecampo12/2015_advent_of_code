b2b0e9e7c7b5a1de14bea00d9b89da344d34dfd4
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-31 12:03:38 -0500

    Day 24 complete

--- day24/AOC.py
+++ day24/AOC.py
@@ -1,20 +1,49 @@
-import re
+from itertools import combinations
+from functools import reduce
+from time import perf_counter
 
-def parse_input(input):
-    return input
-
+# Assumptions:
+#   1. the sum of the weights is divisible by 3
+#   2. It's possible to split the weights into 3 equally weighted groups
+#   3. There are no weights that make up exactly 1/3 of the total weight
+#   4. There at leats 2 weights that make up 1/3 of the total weight
+#   5. No more than half of the weights make up 1/3 of the total weight
 def part1(input):
-    return True
+    input = [int(x) for x in input]
+    g1 = []
+    g2 = []
+    for i in range(2, len(input)//2):
+        for comb in combinations(input, i):
+            if sum(comb) == sum(input)//3:
+                g1.append(comb)
+                break
+    return min([reduce(lambda x, y: x*y, x) for x in g1])
 
 def part2(input):
-    return True
+    input = [int(x) for x in input]
+    g1 = []
+    for i in range(2, len(input)//2):
+        for comb in combinations(input, i):
+            if sum(comb) == sum(input)//4:
+                g1.append(comb)
+                break
+    for g in g1:
+        print(f'Group: {g} QE: {reduce(lambda x, y: x*y, g)}')
+    return min([reduce(lambda x, y: x*y, x) for x in g1])
 
 def main():
-    # input = open("input.txt", "r").read().splitlines()
-    part1_ans = 0
+    input = open("input.txt", "r").read().splitlines()
+    t1 = perf_counter()
+    part1_ans = part1(input)
+    t2 = perf_counter()
     print(f"Part 1: {part1_ans}")
-    part2_ans = 0
+    print(f'Part 1 time: {t2-t1:.4f}\n')
+    
+    t1 = perf_counter()
+    part2_ans = part2(input)
+    t2 = perf_counter()
     print(f"Part 2: {part2_ans}")
+    print(f'Part 2 time: {t2-t1:.4f}')
 
 if __name__ == "__main__":
     main()
--- day24/test.py
+++ day24/test.py
@@ -6,13 +6,15 @@ class Test(unittest.TestCase):
         file = open("test_input.txt", "r")
         input = file.read().splitlines()
         file.close()
-        self.assertTrue(False)
+        ans = part1(input)
+        self.assertEqual(ans, 88)
 
     def test_part2(self):
         file = open("test_input.txt", "r")
         input = file.read().splitlines()
         file.close()
-        self.assertTrue(False)
+        ans = part2(input)
+        self.assertEqual(ans, 33)
         
 if __name__ == "__main__":
     unittest.main()
