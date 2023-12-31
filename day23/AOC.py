09d2c60a0e2a9fd33efd6fc97760eb313b7b6427
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-31 00:23:33 -0500

    Day 23 complete

--- day23/AOC.py
+++ day23/AOC.py
@@ -1,20 +1,46 @@
-import re
-
-def parse_input(input):
-    return input
-
-def part1(input):
-    return True
+def part1(input, part2=False):
+    registers = {"a": 0, "b": 0}
+    if part2:
+        registers["a"] = 1
+    i = 0
+    while i < len(input):
+        inst = input[i].split()
+        match inst[0]:
+            case "hlf":
+                registers[inst[1]] /= 2
+                i += 1
+            case "tpl":
+                registers[inst[1]] *= 3
+                i += 1
+            case "inc":
+                registers[inst[1]] += 1
+                i += 1
+            case "jmp":
+                i += int(inst[1])
+                continue
+            case "jie":
+                if registers[inst[1][0]] % 2 == 0:
+                    i += int(inst[2])
+                    continue
+                else:
+                    i += 1
+            case "jio":
+                if registers[inst[1][0]] == 1:
+                    i += int(inst[2])
+                    continue
+                else:
+                    i += 1
+    return registers
 
 def part2(input):
-    return True
+    return part1(input, part2=True)
 
 def main():
-    # input = open("input.txt", "r").read().splitlines()
-    part1_ans = 0
-    print(f"Part 1: {part1_ans}")
-    part2_ans = 0
-    print(f"Part 2: {part2_ans}")
+    input = open("input.txt", "r").read().splitlines()
+    part1_ans = part1(input)
+    print(f"Part 1: {part1_ans['b']}")
+    part2_ans = part2(input)
+    print(f"Part 2: {part2_ans['b']}")
 
 if __name__ == "__main__":
     main()
--- day23/test.py
+++ day23/test.py
@@ -6,13 +6,15 @@ class Test(unittest.TestCase):
         file = open("test_input.txt", "r")
         input = file.read().splitlines()
         file.close()
-        self.assertTrue(False)
+        ans = part1(input)
+        self.assertEqual(ans['a'], 2)
 
     def test_part2(self):
         file = open("test_input.txt", "r")
         input = file.read().splitlines()
         file.close()
-        self.assertTrue(False)
+        ans = part2(input)
+        self.assertEqual(ans['a'], 7)
         
 if __name__ == "__main__":
     unittest.main()
