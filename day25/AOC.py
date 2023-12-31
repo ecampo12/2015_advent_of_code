9ef9296eab55881153193d788928cd8f3c929316
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-31 17:01:01 -0500

    Day 25 update

--- day25/AOC.py
+++ day25/AOC.py
@@ -1,31 +1,27 @@
 import re
-from math import sqrt, floor
 from time import perf_counter
 
-def get_next_coordinate(coord):
-    if coord[0] == 1:
-        return (coord[1] + 1, 1)
-    else:
-        return (coord[0] - 1, coord[1] + 1)
+def get_triangle_code(coord):
+    row, col = coord
+    triangle = (row + col-1) * (row + col) // 2
+    return triangle - row + 1
 
-# I know that the way nmubers are generated is a triangle number, but I don't know how to calculate it.
 # I'm sure theres a way to calculate repeated multiplication and modulos, but I don't know how to do that.
-def part1(start, start_coord, end_coord):
-    while (start_coord) != end_coord:
-        start_coord = get_next_coordinate(start_coord)
+def part1(end_coord):
+    iter = get_triangle_code(end_coord)
+    start = 20151125
+    for _ in range(iter-1):
         start = (start * 252533) % 33554393
 
     return start
 
 def main():
     input = open("input.txt", "r").read()
-    start = 27995004
-    start_coord = (6, 6)
     row, col = re.findall(r"(\d+)", input)
     end_coord = (int(row), int(col))
 
     t1 = perf_counter()
-    part1_ans = part1(start, start_coord, end_coord)
+    part1_ans = part1(end_coord)
     t2 = perf_counter()
     print(f"Part 1: {part1_ans}")
     print(f'Time: {t2 - t1:.4f} seconds')
--- day25/test.py
+++ day25/test.py
@@ -2,11 +2,15 @@ import unittest
 from AOC import *
 
 class Test(unittest.TestCase):
+    def test_coord_pos(self):
+        coords = [(1, 1), (2, 1), (1, 2), (1, 6), (4, 3), (4, 1), (3, 3), (3, 4), (2, 2)]
+        expected = [1, 2, 3, 21, 18, 7, 13, 19, 5]
+        results = [get_triangle_code(coord) for coord in coords]
+        self.assertEqual(results, expected)
+        
     def test_part1(self):
-        start = 20151125
-        start_coord = (1, 1)
         end_coord = (6, 6)
-        code = part1(start, start_coord, end_coord)
+        code = part1(end_coord)
         self.assertEqual(code, 27995004)
         
 if __name__ == "__main__":
