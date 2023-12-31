54fb702ba9e206497a8b5c4885cea4c6795abae5
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-31 16:53:33 -0500

    Day 25 complete

--- day25/AOC.py
+++ day25/AOC.py
@@ -1,20 +1,34 @@
 import re
+from math import sqrt, floor
+from time import perf_counter
 
-def parse_input(input):
-    return input
+def get_next_coordinate(coord):
+    if coord[0] == 1:
+        return (coord[1] + 1, 1)
+    else:
+        return (coord[0] - 1, coord[1] + 1)
 
-def part1(input):
-    return True
+# I know that the way nmubers are generated is a triangle number, but I don't know how to calculate it.
+# I'm sure theres a way to calculate repeated multiplication and modulos, but I don't know how to do that.
+def part1(start, start_coord, end_coord):
+    while (start_coord) != end_coord:
+        start_coord = get_next_coordinate(start_coord)
+        start = (start * 252533) % 33554393
 
-def part2(input):
-    return True
+    return start
 
 def main():
-    # input = open("input.txt", "r").read().splitlines()
-    part1_ans = 0
+    input = open("input.txt", "r").read()
+    start = 27995004
+    start_coord = (6, 6)
+    row, col = re.findall(r"(\d+)", input)
+    end_coord = (int(row), int(col))
+
+    t1 = perf_counter()
+    part1_ans = part1(start, start_coord, end_coord)
+    t2 = perf_counter()
     print(f"Part 1: {part1_ans}")
-    part2_ans = 0
-    print(f"Part 2: {part2_ans}")
+    print(f'Time: {t2 - t1:.4f} seconds')
 
 if __name__ == "__main__":
     main()
--- day25/test.py
+++ day25/test.py
@@ -3,16 +3,11 @@ from AOC import *
 
 class Test(unittest.TestCase):
     def test_part1(self):
-        file = open("test_input.txt", "r")
-        input = file.read().splitlines()
-        file.close()
-        self.assertTrue(False)
-
-    def test_part2(self):
-        file = open("test_input.txt", "r")
-        input = file.read().splitlines()
-        file.close()
-        self.assertTrue(False)
+        start = 20151125
+        start_coord = (1, 1)
+        end_coord = (6, 6)
+        code = part1(start, start_coord, end_coord)
+        self.assertEqual(code, 27995004)
         
 if __name__ == "__main__":
     unittest.main()
